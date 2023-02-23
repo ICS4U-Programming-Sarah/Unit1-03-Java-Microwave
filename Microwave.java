5b3f7943a4b4ed8c45f96272f049bf06fd0a09f7
Sarah Andrew <sarah.andrew@stu.ocsb.ca>
2023-02-23 03:03:11 +0000

    Lalala

--- Microwave.java
+++ Microwave.java
@@ -32,8 +32,8 @@ public final class Microwave {
 
     public static void main(String[] args) {
         // declare variables
-        final String food1 = "pizza";
-        final String food2 = "sub";
+        final String op_1 = "pizza";
+        final String op_2 = "sub";
         final String food3 = "soup";
         final float TIME_AVE_SUB = 60;
         final float TIME_AVE_PIZ = 45;
@@ -48,7 +48,7 @@ public final class Microwave {
         System.out.println("Are you heating sub, pizza, or soup? ");
         final String userFood = scanner.nextLine();
 
-        if (userFood.equals(food1)) {
+        if (userFood.equals(op_1)) {
             System.out.println("How many items would you like to heat (max 3): ");
             final String amountStr = scanner.nextLine();
 
@@ -63,12 +63,39 @@ public final class Microwave {
                 } else if (amountInt == 3) {
                     time = TIME_AVE_PIZ + TIME_AVE_PIZ;
                     System.out.println("The total cook time is: " + time);
+                } else {
+                    System.out.println("Please enter a valid input");
                 }
             } catch (NumberFormatException error) {
                 // Displays error to user.
                 System.out.print("Please enter valid input."
                         + error.getMessage());
             }
+        }
+        if (userFood.equals(op_2)) {
+            System.out.println("How many items would you like to heat (max 3): ");
+            final String amountStr = scanner.nextLine();
+
+            try {
+                final Integer amountInt = Integer.parseInt(amountStr);
+                if (amountInt == 1) {
+                    time = TIME_AVE_SUB;
+                    System.out.println("The total cook time is: " + time);
+                } else if (amountInt == 2) {
+                    time = (TIME_AVE_SUB * HALF_TIME) + TIME_AVE_SUB;
+                    System.out.println("The total cook time is: " + time);
+                } else if (amountInt == 3) {
+                    time = TIME_AVE_SUB + TIME_AVE_SUB;
+                    System.out.println("The total cook time is: " + time);
+                } else {
+                    System.out.println("Please enter a valid input");
+                }
+            } catch (NumberFormatException error) {
+                // Displays error to user.
+                System.out.print("Please enter valid input."
+                        + error.getMessage());
+            }
+
         }
         scanner.close();
     }
